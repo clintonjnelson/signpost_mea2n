3da7976f892923aee6faba3c24dd0cc0102a2225
clintonjnelson <clintonjnelson@live.com>
2017-01-23 22:51:40 -0800

    get search results somewhat working (doesn't look great, but loads)

--- app/shared/search/search-results/search-results.component.ts
+++ app/shared/search/search-results/search-results.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export class Sign {
+  bgColor: string;
+  icon: string;
+  userId: string;    // THIS MIGHT BE NUMBER INSTEAD - CHECK API RESPONSE
+  knownAs: string;
+}
+
+// SAME THING AS ANOTHER COMPONENT HAS, SO BREAK OUT & SHARE
+export class User {
+  username: string;
+}
 
 @Component({
   moduleId: module.id,
@@ -7,4 +19,11 @@ import { Component } from '@angular/core';
   styleUrls:  ['search-results.component.css']
 })
 
-export class SearchResultsComponent {}
+export class SearchResultsComponent {
+  @Input() users: User[];
+  @Input() signs: Sign[];
+
+  iconClass(iconName: string, size: string = '4'): string {
+    return ('fa fa-' + iconName + ' fa-' + size);
+  }
+}
--- app/styleguide/livingstyleguide.component.ts
+++ app/styleguide/livingstyleguide.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
+import { User, Sign } from '../shared/search/search-results/search-results.component';
 
-@Component({
+const USERS: User[] = [{username: 'Jen'}, {username: 'clint'}];
+const SIGNS: Sign[] = [
+    { bgColor: 'purple', icon: 'instagram', userId: 'jenstagram', knownAs: 'Jennie-bones' },
+    { bgColor: 'blue',   icon: 'facebook',  userId: 'nelson',     knownAs: 'Babyface' }
+  ];
+
+;@Component({
   moduleId: module.id,
   selector: 'livingstyleguide',
   templateUrl: 'livingstyleguide.component.html',
@@ -9,4 +16,8 @@ import { Component } from '@angular/core';
 
 export class LivingStyleGuideComponent {
   // May need to hardcode things in here for components brought in
+
+  // SearchResultsComponent
+  users = USERS;
+  signs = SIGNS;
 }
