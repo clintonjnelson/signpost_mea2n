5fdf26837a2999507f1b2bd33964a80e8130cd3d
clintonjnelson <clintonjnelson@live.com>
2017-01-27 17:57:17 -0800

    update styleguide with four new sign types

--- app/styleguide/livingstyleguide.component.ts
+++ app/styleguide/livingstyleguide.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
-import { User, Sign } from '../shared/search/search-results/search-results.component';
+import { User } from '../shared/search/search-results/search-results.component';
+import { Sign, EmailSign, PhoneSign } from '../signs/sign.model';
 
 const USERS: User[] = [{username: 'Jen'}, {username: 'clint'}];
-const SIGNS: Sign[] = [
-    { bgColor: 'purple', icon: 'instagram', userId: 'jenstagram', knownAs: 'Jennie-bones' },
-    { bgColor: 'blue',   icon: 'facebook',  userId: 'nelson',     knownAs: 'Babyface' }
-  ];
+const DEFAULT_FACEBOOK_SIGN: Sign = {
+    _id: '12345',
+    bgColor: 'red',
+    description: 'some sign',
+    icon: 'facebook-official',
+    knownAs: 'my name',
+    linkUrl: 'http://facebook.com',
+    picUrl: 'http://facebook.com/image',
+    signName: 'facebook',
+    signType: 'facebook',
+    username: 'myactualusername',
+  };
+const CUSTOM_ETSY_SIGN: Sign = {
+    _id: '54321',
+    bgColor: 'orange',
+    description: 'etsy sign',
+    icon: 'etsy',
+    knownAs: 'my etsy name',
+    linkUrl: 'http://etsy.com',
+    picUrl: 'http://etsy.com/image',
+    signName: 'etsy',
+    signType: 'custom',
+    username: 'myetsyusername',
+};
+const EMAIL_SIGN: EmailSign = {
+    _id: '111111',
+    bgColor: 'green',
+    description: 'email sign',
+    icon: 'email',
+    knownAs: 'my email name',
+    linkUrl: '',
+    picUrl: 'http://facebook.com/image',
+    signName: 'email',
+    signType: 'email',
+    username: 'myemailname',
+    email: "fakeemail@example.com"
+};
+const PHONE_SIGN: PhoneSign = {
+    _id: '555555',
+    bgColor: 'blue',
+    description: 'phone sign',
+    icon: 'phone',
+    knownAs: 'my phone name',
+    linkUrl: '',
+    picUrl: 'http://facebook.com/image',
+    signName: 'phone',
+    signType: 'phone',
+    username: 'myphonename',
+    phone: "(555)555-5555"
+};
 
-;@Component({
+const SIGNS: Sign[] = [DEFAULT_FACEBOOK_SIGN, CUSTOM_ETSY_SIGN, EMAIL_SIGN, PHONE_SIGN];
+
+@Component({
   moduleId: module.id,
   selector: 'livingstyleguide',
   templateUrl: 'livingstyleguide.component.html',
@@ -17,7 +66,15 @@ const SIGNS: Sign[] = [
 export class LivingStyleGuideComponent {
   // May need to hardcode things in here for components brought in
 
+
   // SearchResultsComponent
   users = USERS;
   signs = SIGNS;
+
+  // SignComponent
+  customSign = CUSTOM_ETSY_SIGN;
+  defaultSign = DEFAULT_FACEBOOK_SIGN;
+  emailSign   = EMAIL_SIGN;
+  phoneSign   = PHONE_SIGN;
+
 }
