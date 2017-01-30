cbfa3281128e5f45d99805f36f5d1132e5e96dac
clintonjnelson <clintonjnelson@live.com>
2017-01-29 20:42:43 -0800

    add AuthService

--- app/app.module.ts
+++ app/app.module.ts
@@ -12,6 +12,7 @@ import { MaterialModule }  from '@angular/material';
 import { AppComponent }             from './app.component';
 import { SearchBoxComponent }       from './shared/search/search-box/search-box.component';
 import { SearchResultsComponent}    from './shared/search/search-results/search-results.component';
+import { HamnavComponent }          from './shared/navigation/hamnav/hamnav.component';
 import { NavbarComponent }          from './shared/navigation/navbar/navbar.component';
 import { NavLinkComponent }         from './shared/navigation/navlink/navlink.component';
 import { SidenavComponent }         from './shared/navigation/sidenav/sidenav.component';
@@ -25,6 +26,7 @@ import { PhoneSignFormComponent }   from './signs/sign/sign-form/form-types/phon
 
 // Services - make them available EVERYWHERE (otherwise, just add it specifically into Component as a provider)
 import { HelpersService } from './shared/helpers/helpers.service';
+import { AuthService }    from './core/services/auth.service';
 
 @NgModule({
   imports:      [
@@ -38,6 +40,7 @@ import { HelpersService } from './shared/helpers/helpers.service';
                   NavbarComponent,
                   SearchBoxComponent,
                   SearchResultsComponent,
+                  HamnavComponent,
                   NavLinkComponent,
                   LoginSignupFormComponent,
                   SidenavComponent,
@@ -52,6 +55,7 @@ import { HelpersService } from './shared/helpers/helpers.service';
                   AppComponent,
                 ],
   providers:    [
+                  AuthService,
                   HelpersService,
                 ]
 })
