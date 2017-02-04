89fbe15e7c76fa8d3fe8593c858edb8d398030b9
clintonjnelson <clintonjnelson@live.com>
2017-02-04 14:58:47 -0800

    add constructor to auth so vars can be used in components

--- app/core/services/auth.service.ts
+++ app/core/services/auth.service.ts
@@ -3,17 +3,28 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class AuthService {
-  isLoggedIn: boolean = false;
+  isLoggedIn: boolean;
+  isLoggedOut: boolean;
+
+  constructor() {
+    this.isLoggedIn = !!window.localStorage.getItem('auth_key');
+    this.isLoggedOut = !this.isLoggedIn;
+  }
+
+  toggleIsLoggedInOut() {
+    this.isLoggedIn = !this.isLoggedIn;  // Toggle
+    this.isLoggedOut = !this.isLoggedIn; // Opposite
+  }
 
   login() {
     console.log("AUTH LOGIN CLICKED");
     window.localStorage.setItem('auth_key', 'supersecretkey');
-    this.isLoggedIn = true;
+    this.toggleIsLoggedInOut()
   }
 
   logout() {
     console.log("AUTH LOGOUT CLICKED");
     window.localStorage.setItem('auth_key', '');
-    this.isLoggedIn = false;
+    this.toggleIsLoggedInOut();
   }
 }
